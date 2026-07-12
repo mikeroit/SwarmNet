9c758525711c3ae0ae1dc9dbbfe3fd92acfab334
mikeroit <mikeroit@gmail.com>
2026-07-12 14:04:34 -0600

    add LineSegment to geometry lib

---
+++ packages/simulation/src/math/line_segment.rs
@@ -0,0 +1,6 @@
+use crate::math::Point2;
+
+pub struct LineSegment {
+    pub start: Point2,
+    pub end: Point2,
+}
--- packages/simulation/src/math/mod.rs
+++ packages/simulation/src/math/mod.rs
@@ -1,7 +1,9 @@
 pub mod circle;
 pub mod point2;
 pub mod vector2;
+pub mod line_segment;
 
 pub use circle::Circle;
 pub use point2::Point2;
 pub use vector2::Vector2;
+pub use line_segment::LineSegment;
