34799b14e2b90414df0bd1bb3d3a45075582880e
mikeroit <mikeroit@gmail.com>
2026-07-13 18:51:01 -0600

    add ctor for LineSegment

--- packages/simulation/src/math/line_segment.rs
+++ packages/simulation/src/math/line_segment.rs
@@ -4,3 +4,12 @@ pub struct LineSegment {
     pub start: Point2,
     pub end: Point2,
 }
+
+impl LineSegment {
+    pub fn new(start: Point2, end: Point2) -> Self {
+        LineSegment {
+            start,
+            end,
+        }
+    }
+}
